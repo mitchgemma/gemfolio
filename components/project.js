f5310496d7a6d3da636844d860055950de645c64
Mitch <mitchgem@Julies-MacBook-Air-2.local>
2024-02-23 12:44:57 -0700

    adds initial work page

---
+++ components/project.js
@@ -0,0 +1,7 @@
+import styles from '@/styles/components/Work.module.scss';
+
+
+const project = ({ text, position, link, speed }) => {
+};
+
+export default project;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,6 +1,11 @@
+import styles from '@/styles/pages/Work.module.scss'
 
 const work = () => {
-    
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.header}>Recent Work</h1>
+        </div>
+    )
 };
 
 export default work;
